7baffdb584a9ef8277f6fd9fca99d31c5df1fb53
Palealmond <trss95@outlook.com>
2023-04-04 14:37:50 -0400

    Very secure

--- your_space/settings.py
+++ your_space/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)oclqb)$&tcqmuh2u_=uiok2nl%286#f^6h%t1n5k=g+#-h9z'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
