c63271f0a7e3949dca488fdfa01c48aae55f17e8
Palealmond <trss95@outlook.com>
2023-04-04 15:25:45 -0400

    God help

--- your_space/urls.py
+++ your_space/urls.py
@@ -20,8 +20,8 @@ from your_space_app import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
-
 
 
 urlpatterns = [
@@ -29,6 +29,7 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     path('profiles/', views.profile_list),
     path('profiles/<int:pk>/', views.profile_detail),
