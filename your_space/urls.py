5dd39dd6d4bb70076ae3f47178ea23cc29c9e730
Palealmond <trss95@outlook.com>
2023-04-04 11:27:03 -0400

    Maybe fixed

--- your_space/urls.py
+++ your_space/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register('profiles', views.ProfileViewSet)
+router.register('friend_requests', views.FriendRequestViewSet)
+router.register('friendships', views.FriendshipViewSet)
+router.register('posts', views.PostViewSet)
+router.register('comments', views.CommentViewSet)
+router.register('likes', views.LikeViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', include(router.urls)),
 
 ]
