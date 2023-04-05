7d6b1446fd61154078c85bb5e7f428a49f6954b1
Palealmond <trss95@outlook.com>
2023-04-05 12:15:53 -0400

    Re did da backend

--- your_space_app/views.py
+++ your_space_app/views.py
@@ -1,204 +1,48 @@
-
-# Create your views here.
-
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
 from .models import Profile, FriendRequest, Friendship, Post, Comment, Like
 from .serializers import (
     ProfileSerializer, FriendRequestSerializer, FriendshipSerializer,
     PostSerializer, CommentSerializer, LikeSerializer
 )
 
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
-@api_view(['GET', 'POST'])
-def profile_list(request):
-    if request.method == 'GET':
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.objects.all()
+    serializer_class = FriendRequestSerializer
+
+class FriendshipViewSet(viewsets.ModelViewSet):
+    queryset = Friendship.objects.all()
+    serializer_class = FriendshipSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        like = self.get_object()
+        like.users.add(request.user)
+        like.save()
+        serializer = self.get_serializer(like)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-   
-
-
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def profile_detail(request, pk):
-    profile = get_object_or_404(Profile, pk=pk)
-    if request.method == 'GET':
-        serializer = ProfileSerializer(profile)
+    @action(detail=True, methods=['post'])
+    def unlike(self, request, pk=None):
+        like = self.get_object()
+        like.users.remove(request.user)
+        like.save()
+        serializer = self.get_serializer(like)
         return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        profile.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def friend_request_list(request):
-    if request.method == 'GET':
-        friend_requests = FriendRequest.objects.all()
-        serializer = FriendRequestSerializer(friend_requests, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = FriendRequestSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def friend_request_detail(request, pk):
-    friend_request = get_object_or_404(FriendRequest, pk=pk)
-    if request.method == 'GET':
-        serializer = FriendRequestSerializer(friend_request)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = FriendRequestSerializer(friend_request, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        friend_request.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def friendship_list(request):
-    if request.method == 'GET':
-        friendships = Friendship.objects.all()
-        serializer = FriendshipSerializer(friendships, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = FriendshipSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def friendship_detail(request, pk):
-    friendship = get_object_or_404(Friendship, pk=pk)
-    if request.method == 'GET':
-        serializer = FriendshipSerializer(friendship)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = FriendshipSerializer(friendship, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        friendship.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def post_list(request):
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == 'GET':
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def comment_list(request):
-    if request.method == 'GET':
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def comment_detail(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    if request.method == 'GET':
-        serializer = CommentSerializer(comment)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CommentSerializer(comment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        comment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'POST'])
-def like_list(request):
-    if request.method == 'GET':
-        likes = Like.objects.all()
-        serializer = LikeSerializer(likes, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = LikeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def like_detail(request, pk):
-    like = get_object_or_404(Like, pk=pk)
-    if request.method == 'GET':
-        serializer = LikeSerializer(like)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = LikeSerializer(like, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        like.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
